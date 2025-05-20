b493c006dae5a85fefad7b5f8a3f45981322b7d5
Mauricio-Munoz <mauricio.m@moabits.com>
2025-05-19 19:41:03 -0500

    array

---
+++ src/Clase6/EstructuraDeDatos.java
@@ -0,0 +1,33 @@
+package Clase6;
+
+public class EstructuraDeDatos {
+    public static void main(String[] args) {
+
+        // Nombre de estructuras basicas.
+        // Arreglos,listas doblemente enlazadas, map, listas , colas,  tuplas, pilas, , grafos, arboles.
+
+        // ARREGLOS
+        //  finitos. NO ES DINAMICA.
+        // CONDICION 1- YO DEBO DEFINIR EL TAMAÑO
+        // CONDICION 2- DEBO DEFINIR EL TIPO DE DATO
+
+        int[] miAreglo= new int[4];
+        System.out.println(miAreglo[0]);
+        System.out.println(miAreglo[1]);
+        System.out.println(miAreglo[2]);
+        System.out.println(miAreglo[3]);
+        System.out.println("---------------------");
+        miAreglo[0]=10;
+        miAreglo[1]=1;
+        miAreglo[2]=10;
+        miAreglo[3]=11;
+        System.out.println(miAreglo[0]);
+        System.out.println(miAreglo[1]);
+        System.out.println(miAreglo[2]);
+        System.out.println(miAreglo[3]);
+
+
+
+
+    }
+}
