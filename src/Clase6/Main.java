999140974db93cbb692fc64c889617bb8fd62001
Mauricio-Munoz <mauricio.m@moabits.com>
2025-05-19 19:20:45 -0500

    swich moderno

--- src/Clase6/Main.java
+++ src/Clase6/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         int diaDeLaSemana= sc.nextInt();
 
-        switch (diaDeLaSemana){
+     switch (diaDeLaSemana){
             case 1:
                 System.out.println("Lunes");
                 break;
@@ -38,5 +38,16 @@ public class Main {
                 System.out.println("El numero de la semana no existe");
 
         }
+
+        String respuesta = switch (diaDeLaSemana){
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miercoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            case 6,7 -> "Fin de semana";
+            default -> "El numero de la semana no existe";
+        };
+        System.out.println(respuesta);
     }
 }
