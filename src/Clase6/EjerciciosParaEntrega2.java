5484a54dcf5a84587da76f4184e29e18a637c914
Mauricio-Munoz <mauricio.m@moabits.com>
2025-05-19 20:01:59 -0500

    ejercicio

---
+++ src/Clase6/EjerciciosParaEntrega2.java
@@ -0,0 +1,10 @@
+package Clase6;
+
+/*
+1)Escribe un programa que lea un número y determine si es positivo, negativo o cero.
+2)Solicita dos números al usuario y muestra cuál es mayor o si son iguales.
+3)Escribe un programa que determine si un año es bisiesto.
+4)Crea una mini calculadora que pida dos números y un operador (+, -, *, /) y muestre el resultado.*/
+
+public class EjerciciosParaEntrega2 {
+}
